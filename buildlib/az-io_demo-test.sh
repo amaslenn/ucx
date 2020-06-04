6d6cb6fabba53f0a911aa62f9c6c02b73f964714
Andrey Maslennikov <andreyma@mellanox.com>
2020-06-04 15:19:50 +0300

    CI: run client in foreground

--- buildlib/az-io_demo-test.sh
+++ buildlib/az-io_demo-test.sh
@@ -25,26 +25,6 @@ ip addr show bond0
 ping -c 2 $IP
 
 source "${workspace}/../test/apps/iodemo/env"
-if ! "${workspace}/../test/apps/iodemo/io_demo" -l $timeout -i 10000000 $IP >client.log 2>&1 & then
-    cat client.log
+if ! "${workspace}/../test/apps/iodemo/io_demo" -l $timeout -i 10000000 $IP; then
     error "Failed to start client"
-fi
-client_pid=$!
-
-# double check the process is running
-sleep 3
-if ! kill -0 $client_pid; then
-    cat client.log
-    error "Error in client"
-fi
-echo "Client is running, PID=$client_pid"
-
-# TODO: implement network corruptions
-
-if ! wait $client_pid; then
-    cat client.log
-    error "Error in client"
-else
-    cat client.log
-    echo "Client exited successfully"
 fi
