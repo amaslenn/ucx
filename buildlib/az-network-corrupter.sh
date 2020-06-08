1e92998e6b4538b4e30240b3a1ec34cd14693c43
Andrey Maslennikov <andreyma@mellanox.com>
2020-06-08 16:12:39 +0300

    CI: run real corrupter

--- buildlib/az-network-corrupter.sh
+++ buildlib/az-network-corrupter.sh
@@ -2,14 +2,26 @@
 
 eval "$*"
 initial_delay=${initial_delay:=10}
-cycles=${cycles:=2}
+cycles=${cycles:=1000}
 downtime=${downtime:=5}
 uptime=${uptime:=20}
+reset=${reset:="no"}
+
+
+if [ "x$reset" = "xyes" ]; then
+    sudo /hpc/noarch/git_projects/swx_infrastructure/clusters/bin/manage_host_ports.sh "$(hostname)" "bond-up"
+    exit $?
+fi
 
 echo "Initial delay ${initial_delay} sec"
 sleep ${initial_delay}
 
 for i in $(seq 1 ${cycles} 1); do
-    echo "#$i Put it down! And sleep ${downtime}"; sleep "$downtime"
-    echo "#$i Put it up! And sleep ${uptime}"; sleep "$uptime"
+    echo "#$i Put it down! And sleep ${downtime}"
+    sudo /hpc/noarch/git_projects/swx_infrastructure/clusters/bin/manage_host_ports.sh "$(hostname)" "bond-down"
+    sleep "$downtime"
+
+    echo "#$i Put it up! And sleep ${uptime}"
+    sudo /hpc/noarch/git_projects/swx_infrastructure/clusters/bin/manage_host_ports.sh "$(hostname)" "bond-up"
+    sleep "$uptime"
 done
