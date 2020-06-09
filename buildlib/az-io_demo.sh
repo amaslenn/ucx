b50960b78cd44c993b644f12d35b0c1e320bf1df
Andrey Maslennikov <andreyma@mellanox.com>
2020-06-09 11:55:33 +0300

    CI: test #21

--- buildlib/az-io_demo.sh
+++ buildlib/az-io_demo.sh
@@ -34,7 +34,7 @@ if [ "x$server_ip" = "x" ]; then
     if ! "${workspace}/../test/apps/iodemo/io_demo" |& add_timestamp >server.log & then
         error "Failed to start server"
     fi
-    server_pid=$(pgrep -u "$USER" io_demo | awk '{print $2}')
+    server_pid=$(pgrep -u "$USER" -f io_demo)
     echo "Server is running, PID=$server_pid"
     azure_set_variable "server_pid" "$server_pid"
 
